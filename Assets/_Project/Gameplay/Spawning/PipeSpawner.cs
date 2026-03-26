d020cc203922b4f7b092c2346748a7a5c894d169
Nico <nico.klinkert2003@gmail.com>
2026-03-26 01:17:31 +0100

    code refactored

--- Assets/_Project/Gameplay/Spawning/PipeSpawner.cs
+++ Assets/_Project/Gameplay/Spawning/PipeSpawner.cs
@@ -5,9 +5,10 @@ using Random = UnityEngine.Random;
 public class PipeSpawner : MonoBehaviour
 {
     public GameObject pipePrefab;
+    public GameObject scoreColliderPrefab;
 
     public int gapX = 7;
-    public int pipeGapMin = 16;
+    public int pipeGapMin = 16; //the smaller this value, the closer the pipes will get -> game gets harder
     public int pipeGapMax = 8;
 
     private void Start()
@@ -17,13 +18,20 @@ public class PipeSpawner : MonoBehaviour
 
     void SpawnPipe()
     {
+        //TODO a specific amount of pipes will spawn depending on the player socore -> for example every third score new pipes will spawn. this way its endless
         for (int i = 0; i < 20; i++)
         {
-            float offsetTop =  Random.Range(pipeGapMin, pipeGapMax);
-            float offsetBot = -Random.Range(pipeGapMin, pipeGapMax);
+            int offsetTop =  Random.Range(pipeGapMin, pipeGapMax);
+            int offsetBot = -Random.Range(pipeGapMin, pipeGapMax);
+
+            int scoreColliderYPosition = offsetTop / offsetBot;
             
-            GameObject pipeTop = Instantiate(pipePrefab, new Vector3(transform.position.x + i * gapX, offsetTop, 0), Quaternion.identity);
-            GameObject pipeBot = Instantiate(pipePrefab, new Vector3(transform.position.x + i * gapX, offsetBot, 0), Quaternion.identity);   
+            float xPosition = transform.position.x + i * gapX;
+            
+            GameObject pipeTop = Instantiate(pipePrefab, new Vector3(xPosition, offsetTop, 0), Quaternion.identity);
+            GameObject pipeBot = Instantiate(pipePrefab, new Vector3(xPosition, offsetBot, 0), Quaternion.identity); 
+            
+            GameObject scoreCollider = Instantiate(scoreColliderPrefab, new Vector3(xPosition, scoreColliderYPosition, 0), Quaternion.identity);
         }
     }
 }
